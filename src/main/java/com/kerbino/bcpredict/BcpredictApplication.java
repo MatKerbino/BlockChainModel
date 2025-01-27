a302432a8a9402a76e65fb894d424cec9ad77259
Matheus Willian <159083906+MatKerbino@users.noreply.github.com>
2025-01-27 12:52:33 -0300

    Initial commit

---
+++ src/main/java/com/kerbino/bcpredict/BcpredictApplication.java
@@ -0,0 +1,13 @@
+package com.kerbino.bcpredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcpredictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcpredictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kerbino/bcpredict/BcpredictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerbino.bcpredict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcpredictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
